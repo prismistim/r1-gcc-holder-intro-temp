d2c9e520be2e8b7275bba88e3bb4935f0b0faf62
snowsphere <info@snowsphere.net>
2024-05-11 17:37:00 +0900

    WIP

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,27 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    colors: {
+      primary: '#f43f5e',
+      'primary-content': '#262626',
+      secondary: '#ddd',
+      accent: '#2dd4bf',
+      neutral: '#e5e7eb',
+      'base-100': '#f3f4f6',
+      info: '#38bdf8',
+      success: '#4ade80',
+      warning: '#fbbf24',
+      error: '#ef4444',
+      white: '#ffffff'
+    },
+    extend: {
+      fontFamily: {
+        sans: ['"Rubik Variable"', '"M PLUS 2 Variable"', 'sans-serif']
+      }
+    }
+  },
+  plugins: []
 }
